05091aaccdb286956a08e4e9b9486eb8406cb716
Ruslan Israfilov <ruslan.is@outlook.com>
2019-02-05 13:23:51 +0300

    Add third party tools for python/cython

---
+++ WORKSPACE
@@ -0,0 +1,5 @@
+workspace(name = "nestopt")
+
+# Configure Python repository (stores path to the include and lib)
+load("@//third_party:python.bzl", "python_repository")
+python_repository(name = "python")
---
+++ nestopt/core/types.hpp
@@ -0,0 +1,17 @@
+#ifndef NESTOPT_CORE_TYPES_HPP_
+#define NESTOPT_CORE_TYPES_HPP_
+
+#include <vector>
+#include <cstddef>
+
+namespace nestopt {
+namespace core {
+
+using Scalar = double;
+using Size   = std::size_t;
+using Vector = std::vector<Scalar>;
+
+} // namespace nestopt
+} // namespace core
+
+#endif // NESTOPT_CORE_TYPES_HPP_
